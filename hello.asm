e7dc82290e36fe8e2b0dc114dc5530324e2015c9
sky line <32512284+linesky@users.noreply.github.com>
2024-03-24 11:56:03 +0000

    Add files via upload

--- hello.asm
+++ hello.asm
@@ -1,18 +1,63 @@
 format mz
 start:
 		
-write_text:
-	mov ax,cs
-	mov ds,ax
-	mov es,ax	
-	mov dx,string1
-	mov ah,9
-	int 0x21
-exit:
-	mov ax,0x4c00
-	int 0x21
+          mov edi,0b8001h
+          mov ecx,80*26*2
+          mov al,017h
+          mov edx,2
+          call fill32
+          mov edi,0b8000h
+          mov ecx,80*26*2
+          mov al,'*'
+          mov edx,2
+          call fill32                          
+                          
+          exitdo:                
+                                    
+                                    
+                    exit:                
+                              int 20h                
+                              xor ah,ah
+                              int 21h                
+                                             
+                
 halt:
 jmp halt
+fill32:                
+          push eax                
+          push ebx                
+          push ecx                
+          push edx                
+          push esi                
+          push edi                
+          push ebp                
+          push ds                
+          mov bp,0                
+          mov ds,bp
+          cmp edx,0
+          JNZ FILL3211
+          inc edx                
+          FILL3211:
+          FILL321:  
+                    ds    
+                    mov [edi],al
+                    clc                
+                    add edi,edx
+                    dec ecx                
+                    JNZ FILL321
+                    pop ds                
+                    pop ebp                
+                    pop edi                
+                    pop esi                
+                    pop edx                
+                    pop ecx                 
+                    pop ebx                
+                    pop eax                
+                    RET                
+
+x     db 0     
+y     db 0
+color dw 07h
 string1 db "hello world.....",13,10,"$"
 string2 db "$",0
 
